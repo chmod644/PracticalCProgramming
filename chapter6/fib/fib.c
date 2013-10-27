45280c7002e7e163c63dbb4c879a272e2ce9682c
chmod644 <chmod644@github.com>
2013-10-27 21:27:02 +0900

    add chapter6

---
+++ chapter6/fib/fib.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int old_number;
+int current_number;
+int next_number;
+
+int main() {
+	old_number = 1;
+	current_number = 1;
+	while(current_number<100) {
+		printf("%d\n", current_number);
+		next_number = current_number + old_number;
+		old_number = current_number;
+		current_number = next_number;
+	}
+	return(0);
+}
